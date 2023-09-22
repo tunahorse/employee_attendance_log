d694e07780cbdf01ade044e6bd99cf5b5eb636da
Fabian <fabiananguiano@gmail.com>
2023-09-22 15:54:51 -0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Employee Attendance Log',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'summary': 'Log employee attendance and calculate hours worked.',
+    'author': 'Fabian Anguiano',
+    'license': 'MIT',
+    'website': 'https://www.fabiananguiano.com',
+    'depends': ['base', 'hr'],
+    'data': [
+        'views/attendance_log_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
